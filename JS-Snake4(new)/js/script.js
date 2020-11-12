e3701424c8bf86d12a80319aeedcb8b20bdff5e7
Mattia <mattia.tummolilllo@gmail.com>
2020-11-12 16:08:06 +0100

    4

--- JS-Snake4(new)/js/script.js
+++ JS-Snake4(new)/js/script.js
@@ -2,14 +2,25 @@
 // Partendo da un array semplice tipo questo, chiediamo all’utente con 2 prompt due numeri tra 0 e la lunghezza dell’array
 // quindi creiamone uno nuovo con solo i valori che hanno la posizione compresa tra i due numeri inseriti dall’utente
 
+// const myArray = ['Michele', 'Fabio', 'Roberto', 'Giovanni', 'Simone', 'Chiara'];
+// const myArray2 = [];
+//
+// var a = parseInt(prompt("Inserisci un numero tra 0 e 6"));
+// var b = parseInt(prompt("Inserisci un altro numero tra 0 e 6"));
+//
+// var fromTo = myArray.slice(a, b);
+// myArray2.push(fromTo);
+//
+// console.log(myArray);
+// console.log(myArray2);
+
 const myArray = ['Michele', 'Fabio', 'Roberto', 'Giovanni', 'Simone', 'Chiara'];
-const myArray2 = [];
 
 var a = parseInt(prompt("Inserisci un numero tra 0 e 6"));
 var b = parseInt(prompt("Inserisci un altro numero tra 0 e 6"));
 
-var fromTo = myArray.slice(a, b);
-myArray2.push(fromTo);
+const myArray2 = myArray.filter((element, index) => {
+   return element[index] > a && element[index] < b;
+});
 
-console.log(myArray);
 console.log(myArray2);
